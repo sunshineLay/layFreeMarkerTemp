badbb46d727cd24ceb10bcde5194a0b678f3cae3
GitHub Action <action@github.com>
2021-01-25 01:23:26 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sunshinelay/layfreemarkertemp/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sunshinelay.layfreemarkertemp.services
+
+import com.github.sunshinelay.layfreemarkertemp.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
